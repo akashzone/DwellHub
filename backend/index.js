06f9dc351672325ea16ec19c135fa1c1819cbeb2
akashzone <akashnadar.dev@gmail.com>
2026-04-20 15:12:20 +0530

    insert sample data in collection

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,39 @@
+//Imports
 
-
+//Express connection
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
-
+//Mongoose connection
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/DwellHub"
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+const Listing = require("./models/listing.js")
 main().then(()=>{
     console.log("MongoDB connected successfully.")
 }).catch(err => 
     console.log("Err : ",err)
 );
 
-app.get("/sample",(req,res)=>{
-    
+app.get("/sample", async (req,res)=>{
+    const sampleListing = new Listing({
+        title: "New Home",
+        description: "Sweet place to leave.",
+        price: "$199/night",
+        location: "Mumbia,Sion",
+        country: "India"
+    });
+
+    await sampleListing.save().then(()=>{
+        console.log("Successfully inserted sample data!");
+        res.send("Inserted Successfully")
+    }).catch(err => 
+        console.log("Err :",err)
+    )
 })
 
 app.get("/test",(req,res)=>{
@@ -26,7 +41,7 @@ app.get("/test",(req,res)=>{
 });
 
 
-
+//Server listens to port 3000
 app.listen(PORT,(req,res)=>{
     console.log(`Server running on port ${PORT}!`);
 });
---
+++ backend/models/listing.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    }
+})
+
+
+
+module.exports = mongoose.model("Listing", listingSchema);
