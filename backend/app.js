d692708fdd4ef80543765c80d44c4bc94fde3aaf
akashzone <akashnadar.dev@gmail.com>
2026-04-23 13:14:53 +0530

    create show route to show individual listing

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,42 @@
 //Imports
 const path = require("path");
 
-
 //Express connection
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 //Mongoose connection
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb://127.0.0.1:27017/DwellHub"
-async function main(){
-    await mongoose.connect(MONGO_URL);
+const MONGO_URL = "mongodb://127.0.0.1:27017/DwellHub";
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-const Listing = require("./models/listing.js")
-main().then(()=>{
-    console.log("MongoDB connected successfully.")
-}).catch(err => 
-    console.log("Err : ",err)
-);
+const Listing = require("./models/listing.js");
+main()
+  .then(() => {
+    console.log("MongoDB connected successfully.");
+  })
+  .catch((err) => console.log("Err : ", err));
 
 //Middlewares
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname,"views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //show all
-app.get("/listings",async (req,res)=>{
-    let sampleListing = await Listing.find({});
-    // console.log(sampleListing); 
-    res.render("index",{ name:"Akash", sampleListing});
+app.get("/listings", async (req, res) => {
+  let sampleListing = await Listing.find({});
+  res.render("index", { sampleListing });
 });
+
+// show route - shows individual listing
+app.get("/listings/:id", async (req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    console.log(listing);
+    res.render("show.ejs",{listing});
+})
 
 // app.get("/testListing", async (req,res)=>{
 //     const sampleListing = new Listing({
@@ -44,16 +50,16 @@ app.get("/listings",async (req,res)=>{
 //     await sampleListing.save().then(()=>{
 //         console.log("Successfully inserted sample data!");
 //         res.send(sampleListing)
-//     }).catch(err => 
+//     }).catch(err =>
 //         console.log("Err :",err)
 //     )
 // })
 
-app.get("/test",(req,res)=>{
-    res.send("test route is working.");
+app.get("/test", (req, res) => {
+  res.send("test route is working.");
 });
 
 //Server listens to port 3000
-app.listen(PORT,(req,res)=>{
-    console.log(`Server running on port ${PORT}!`);
+app.listen(PORT, (req, res) => {
+  console.log(`Server running on port ${PORT}!`);
 });
